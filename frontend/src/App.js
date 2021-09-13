ee54122107958f25764460b1df072d1c9fba47c3
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-09-13 02:07:14 -0400

    Readded tailwind, removed chakra-ui, added billboard, changes nav css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Nav } from './components/Nav';
-import { Box } from '@chakra-ui/react';
-import { getTrending } from './api/tmdb';
-import { Card } from './components/Card';
+import { Billboard } from './components/Billboard';
 
 export const App = () => {
-  const [trendingList, setTrendingList] = useState([]);
-
-  useEffect(() => {
-    getTrending().then((res) => {
-      console.log(res);
-      setTrendingList(res.data.results);
-    });
-  }, []);
-
   return (
     <div>
       <Nav />
-      <Box disaply="flex" flexDirection="row">
-        {trendingList.map((item) => (
-          <Card data={item}></Card>
-        ))}
-      </Box>
+      <Billboard />
     </div>
   );
 };
+
+/*
+
+    background-image: -webkit-gradient(linear,left top,left bottom,color-stop(10%,rgba(0,0,0,.7)),color-stop(10%,rgba(0,0,0,0)));
+    background-image: -webkit-linear-gradient(top,rgba(0,0,0,.7) 10%,rgba(0,0,0,0));
+    background-image: -moz- oldlinear-gradient(top,rgba(0,0,0,.7) 10%,rgba(0,0,0,0));
+    background-image: -o-linear-gradient(top,rgba(0,0,0,.7) 10%,rgba(0,0,0,0));
+    background-image: linear-gradient(to bottom,rgba(0,0,0,.7) 10%,rgba(0,0,0,0));
+    */
