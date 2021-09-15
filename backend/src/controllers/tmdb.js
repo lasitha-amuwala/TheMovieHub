091ddd60dc4a9552ac585e2d4352cec27fa573d2
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-09-15 03:49:46 -0400

    Added more controllers, routes, api, added category lists and slidinging effect

--- backend/src/controllers/tmdb.js
+++ backend/src/controllers/tmdb.js
@@ -1,8 +1,46 @@
-const {getTrending} = require('../services/TMDB/movies')
+const {
+  getTrending,
+  getPopular,
+  getTopRated,
+  getUpcoming,
+} = require('../services/TMDB/movies');
 
 module.exports.getTrending = async (req, res) => {
   try {
     getTrending().then((data) => {
+      res.status(200).json(data);
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+};
+
+module.exports.getPopular = async (req, res) => {
+  try {
+    getPopular().then((data) => {
+      res.status(200).json(data);
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+};
+
+module.exports.getTopRated = async (req, res) => {
+  try {
+    getTopRated().then((data) => {
+      res.status(200).json(data);
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500);
+  }
+};
+
+module.exports.getUpcoming = async (req, res) => {
+  try {
+    getUpcoming().then((data) => {
       res.status(200).json(data);
     });
   } catch (e) {
