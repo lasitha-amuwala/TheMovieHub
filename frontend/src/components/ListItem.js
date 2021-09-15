46347c10e5830844b2ca54a17efde729893259e5
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-09-15 09:44:22 -0400

    Improved image loading speed

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -5,7 +5,7 @@ export const ListItem = ({data}) => {
     <div className="h-80 select-none">
       <img
         className="h-full w-54 max-w-none ring ring-gray-900 ring-opacity-100 cursor-pointer focus:ring-gray-700 rounded-lg"
-        src={`https://image.tmdb.org/t/p/original/${data.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
         alt={`${data.title.split(' ').join('-')}-poster`}
       />
     </div>
