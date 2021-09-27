2a5faf39dd73472544cf03b18b7de21b825e163d
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-09-26 22:38:46 -0400

    Improved List component

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const ListItem = ({ data }) => (
-  <div className="max-w-1/3 sm:max-w-1/4 md:max-w-1/5 lg:max-w-1/6 xl:max-w-1/7 2xl:max-w-1/8 transform hover:scale-125 transition duration-500 ease-in-out">
+  <div className="max-w-1/3 sm:max-w-1/4 md:max-w-1/5 lg:max-w-1/6 xl:max-w-1/7 2xl:max-w-1/8 transform hover:scale-125 hover:mx-5 z-20 hover:z-30 transition-all duration-500 ease-in-out">
     <img
       className="h-full cursor-pointer rounded-lg"
       src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
