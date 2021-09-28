6e65882e3af845a70d2bd7e79917c61da72eac11
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-09-27 23:58:42 -0400

    Fixed warnings

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export const ListItem = ({ data }) => (
-  <div className="max-w-1/3 sm:max-w-1/4 md:max-w-1/5 lg:max-w-1/6 xl:max-w-1/7 2xl:max-w-1/8 transform hover:scale-125 hover:mx-5 z-20 hover:z-30 transition-all duration-500 ease-in-out">
-    <img
-      className="h-full cursor-pointer rounded-lg"
-      src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-      alt={`${data.title.split(' ').join('-')}-poster`}
-    />
-  </div>
+  <img
+    className=" h-full w-c-1/3 sm:w-c-1/4 md:w-c-1/5 lg:w-c-1/6 xl:w-c-1/7 2xl:w-c-1/8 cursor-pointer rounded-lg"
+    src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+    alt={`${data.title.split(' ').join('-')}-poster`}
+  />
 );
+
+//transform hover:scale-125 hover:-translate-x-2/3 z-20 hover:z-30 transition-all duration-500 ease-in-out
