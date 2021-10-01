1f61a57548674adfc0991e7c7d34a7eb9ec1bfbc
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-10-01 00:04:00 -0400

    Improved slide component

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -5,14 +5,56 @@ import { ReactComponent as ChevronRight } from '../icons/chevronRight.svg';
 import { ReactComponent as ChevronLeft } from '../icons/chevronLeft.svg';
 
 export const List = ({ data, title }) => {
-  //const [distance, setDistance] = useState(0);
-  //const [count, setCount] = useState(0);
-  //const [numToShow, setNumToShow] = useState(0);
-  const listRef = useRef();
+  const listRef = useRef(null);
+  const itemRef = useRef(null);
 
-  const handleLeftClick = () => {};
-  const handleRightClick = () => {
-    listRef.current.style.transform = `translateX(${-window.innerWidth}px)`;
+  const [itemsDisplayed, setItemsDisplayed] = useState(0);
+  const [num, setNum] = useState(itemsDisplayed);
+  const [count, setCount] = useState(0);
+
+  const handleResize = () => {
+    const windowWidth = window.innerWidth;
+    if (windowWidth > 1536) {
+      setItemsDisplayed(8);
+    } else if (windowWidth > 1280) {
+      setItemsDisplayed(7);
+    } else if (windowWidth > 1024) {
+      setItemsDisplayed(6);
+    } else if (windowWidth > 768) {
+      setItemsDisplayed(5);
+    } else if (windowWidth > 640) {
+      setItemsDisplayed(4);
+    } else {
+      setItemsDisplayed(3);
+    }
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+  }, []);
+
+  const handleClick = (direction) => {
+    let width =
+      itemsDisplayed * itemRef.current.clientWidth + itemsDisplayed * 8;
+      
+    if (direction === 'left') {
+      console.log('left', count, width, count - width);
+      listRef.current.style.transform = `translateX(${count  - width}px)`;
+      setCount(count - width);
+    } else {
+      listRef.current.style.transform = `translateX(${-(count + width)}px)`;
+      setCount(count + width);
+    }
+    console.log('hi' ,count)
+  };
+
+  const handleLeftClick = (width, c) => {
+    console.log('y', count, width);
+    listRef.current.style.transform = `translateX(${c + width}px)`;
+  };
+  const handleRightClick = (width, c) => {
+    listRef.current.style.transform = `translateX(${-(c + width)}px)`;
   };
 
   return (
@@ -24,13 +66,13 @@ export const List = ({ data, title }) => {
         <div className="w-4% 2xl:w-14 h-full rounded-r-lg cursor-pointer bg-black absolute left-0 z-50 bg-opacity-50 hover:bg-opacity-70">
           <ChevronLeft
             className="w-full h-full opacity-0 group-hover:opacity-100"
-            onClick={() => handleLeftClick()}
+            onClick={() => handleClick('left')}
           />
         </div>
         <div className="w-4% 2xl:w-14 h-full rounded-l-lg cursor-pointer bg-black absolute right-0 z-50 bg-opacity-50 hover:bg-opacity-70">
           <ChevronRight
             className="w-full h-full opacity-0 group-hover:opacity-100"
-            onClick={() => handleRightClick()}
+            onClick={() => handleClick('right')}
           />
         </div>
         <div
@@ -38,7 +80,7 @@ export const List = ({ data, title }) => {
           className="flex m-ml-4% 2xl:ml-14 w-max gap-2 h-full transform transition duration-300 ease-linear "
         >
           {data.map((item) => (
-            <ListItem data={item} />
+            <ListItem data={item} key={item.id} ref={itemRef} />
           ))}
         </div>
       </div>
