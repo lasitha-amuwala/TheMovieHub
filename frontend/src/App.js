cc9b6f499f002f08605b5d331f66b7df0da79ab7
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-10-09 23:04:55 -0400

    Added home page directory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { getTrending, getPopular, getTopRated, getUpcoming } from './api/tmdb';
-
-import { Navbar } from './components/Navbar';
-import { Featured } from './components/Carousel/Featured';
-import { List } from './components/List';
+import React from 'react';
+import { Home } from './pages/Home/Home';
 
 export const App = () => {
-	const [trendingList, setTrendingList] = useState([]);
-	const [popularList, setPopularList] = useState([]);
-	const [topRatedList, setTopRatedList] = useState([]);
-	const [upcomingList, setUpcomingList] = useState([]);
-
-	useEffect(() => {
-		getTrending().then((res) => setTrendingList(res.data.results));
-		getPopular().then((res) => setPopularList(res.data.results));
-		getTopRated().then((res) => setTopRatedList(res.data.results));
-		getUpcoming().then((res) => setUpcomingList(res.data.results));
-	}, []);
-
-	return (
-		<div className="overflow-hidden min-h-screen relative pb-16 mt-16">
-			<Navbar />
-			<Featured slides={trendingList} autoplay/>
-			<List data={trendingList} title="Trending Now" />
-			<List data={popularList} title="Popular" />
-			<List data={topRatedList} title="Top Rated" />
-			<List data={upcomingList} title="Upcoming" />
-
-			<div className="absolute bottom-0 w-full h-16 bg-black bg-opacity-30"></div>
-		</div>
-	);
+	return <Home />;
 };
---
+++ frontend/src/pages/Home/Home.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import { getTrending, getPopular, getTopRated, getUpcoming } from '../../api/tmdb';
+
+import { Navbar } from '../../components/Navbar';
+import { Featured } from './Carousel/Featured';
+import { List } from '../../components/List';
+
+export const Home = () => {
+	const [trendingList, setTrendingList] = useState([]);
+	const [popularList, setPopularList] = useState([]);
+	const [topRatedList, setTopRatedList] = useState([]);
+	const [upcomingList, setUpcomingList] = useState([]);
+
+	useEffect(() => {
+		getTrending().then((res) => setTrendingList(res.data.results));
+		getPopular().then((res) => setPopularList(res.data.results));
+		getTopRated().then((res) => setTopRatedList(res.data.results));
+		getUpcoming().then((res) => setUpcomingList(res.data.results));
+	}, []);
+
+	return (
+		<div className="overflow-hidden min-h-screen relative pb-16 mt-16">
+			<Navbar />
+			<Featured slides={trendingList} autoplay/>
+			<List data={trendingList} title="Trending Now" />
+			<List data={popularList} title="Popular" />
+			<List data={topRatedList} title="Top Rated" />
+			<List data={upcomingList} title="Upcoming" />
+
+			<div className="absolute bottom-0 w-full h-16 bg-black bg-opacity-30"></div>
+		</div>
+	);
+};
