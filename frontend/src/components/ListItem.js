ea1b3653eb2cd3f7222a8424f669c0f618ea6fbd
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-10-21 20:03:36 -0400

    Created CarouselItem component, remove unused code

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -5,7 +5,7 @@ export const ListItem = forwardRef((props, ref) => {
   return (
     <img
       ref={ref}
-      className=" h-full w-c-1/3 sm:w-c-1/4 md:w-c-1/5 lg:w-c-1/6 xl:w-c-1/7 2xl:w-c-1/8 cursor-pointer rounded-lg"
+      className="h-full w-c-1/3 sm:w-c-1/4 md:w-c-1/5 lg:w-c-1/6 xl:w-c-1/7 2xl:w-c-1/8 cursor-pointer rounded-lg"
       src={`https://image.tmdb.org/t/p/w500/${props.data.poster_path}`}
       alt={`${props.data.title.split(' ').join('-')}-poster`}
     />
