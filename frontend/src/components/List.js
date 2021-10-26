7f7bb89374825d5c8bd27e7cbd8211c445501aa3
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-10-26 19:38:15 -0400

    Fixed list navigation, improved list style, increased list button size

--- frontend/src/components/List.js
+++ frontend/src/components/List.js
@@ -8,45 +8,29 @@ export const List = ({ data, title }) => {
 	const listRef = useRef(null);
 	const itemRef = useRef(null);
 
-	const [itemsDisplayed, setItemsDisplayed] = useState(0);
-	const [num, setNum] = useState(itemsDisplayed);
 	const [count, setCount] = useState(0);
+	const [lastIndex, setLastIndex] = useState(0);
 
-	const handleResize = () => {
-		const windowWidth = window.innerWidth;
-		if (windowWidth > 1536) {
-			setItemsDisplayed(8);
-		} else if (windowWidth > 1280) {
-			setItemsDisplayed(7);
-		} else if (windowWidth > 1024) {
-			setItemsDisplayed(6);
-		} else if (windowWidth > 768) {
-			setItemsDisplayed(5);
-		} else if (windowWidth > 640) {
-			setItemsDisplayed(4);
-		} else {
-			setItemsDisplayed(3);
+	const handleClick = (direction) =>
+		setCount((count) => (direction === 'left' ? count - 1 : count + 1));
+
+	const handleSlide = () => {
+		if (listRef.current && itemRef.current) {
+			let len = data.length;
+			let num = Math.floor(
+				listRef.current.clientWidth / itemRef.current.clientWidth
+			);
+
+			setLastIndex(Math.ceil(len / num) - 1);
+
+			let remainder = len - num * count;
+			let translate = remainder < num ? count - 1 + remainder / num : count;
+			listRef.current.style.transform = `translateX(${translate * -100}%)`;
 		}
 	};
 
-	useEffect(() => {
-		handleResize();
-		window.addEventListener('resize', handleResize);
-	}, []);
-
-	const handleClick = (direction) => {
-		let width =
-			itemsDisplayed * itemRef.current.clientWidth + itemsDisplayed * 8;
-
-		if (direction === 'left') {
-			console.log('left', count, width, count - width);
-			listRef.current.style.transform = `translateX(${count - width}px)`;
-			setCount(count - width);
-		} else {
-			listRef.current.style.transform = `translateX(${-(count + width)}px)`;
-			setCount(count + width);
-		}
-	};
+	useEffect(handleSlide, [count]);
+	useEffect(() => window.addEventListener('resize', () => setCount(0)), []);
 
 	return (
 		<div className="group mt-3 mb-2 text-white w-full">
@@ -54,56 +38,33 @@ export const List = ({ data, title }) => {
 				{title}
 			</span>
 			<div className="mt-3 relative">
-				<div className="w-4% 2xl:w-14 h-full rounded-r-lg cursor-pointer bg-black absolute left-0 z-50 bg-opacity-50 hover:bg-opacity-70">
-					<ChevronLeft
-						className="w-full h-full opacity-0 group-hover:opacity-100"
-						onClick={() => handleClick('left')}
-					/>
-				</div>
-				<div className="w-4% 2xl:w-14 h-full rounded-l-lg cursor-pointer bg-black absolute right-0 z-50 bg-opacity-50 hover:bg-opacity-70">
-					<ChevronRight
-						className="w-full h-full opacity-0 group-hover:opacity-100"
-						onClick={() => handleClick('right')}
-					/>
-				</div>
-					<div
+				{count > 0 && (
+					<div className="w-4% 2xl:w-13 h-full rounded-r-lg cursor-pointer bg-black absolute left-0 z-50 bg-opacity-50 hover:bg-opacity-70">
+						<ChevronLeft
+							className="w-full h-full opacity-0 group-hover:opacity-100"
+							onClick={() => handleClick('left')}
+						/>
+					</div>
+				)}
+				{(count === 0 || count !== lastIndex) && (
+					<div className="w-4% 2xl:w-13 h-full rounded-l-lg cursor-pointer bg-black absolute right-0 z-50 bg-opacity-50 hover:bg-opacity-70">
+						<ChevronRight
+							className="w-full h-full opacity-0 group-hover:opacity-100"
+							onClick={() => handleClick('right')}
+						/>
+					</div>
+				)}
+				<div className="px-4% 2xl:px-14 overflow-hidden select-none">
+					<ul
 						ref={listRef}
-						className="flex px-4% 2xl:px-14 w-full gap-2 h-full transition duration-300 ease-linear"
+						className="flex transition duration-700 ease-in-out"
 					>
 						{data.map((item) => (
 							<ListItem data={item} key={item.id} ref={itemRef} />
 						))}
-					</div>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
 };
-
-/*
-  const handleClick = (direction) => {
-    //let distance = listRef.current.getBoundingClientRect().x - 48;
-    //let num = Math.floor((window.innerWidth - 48) / 231);
-    //setNum(num);
-    let width;
-    if (count + numToShow > data.length) {
-      width = (data.length % numToShow) * 231;
-      setCount(numToShow);
-    } else {
-      width = numToShow * 231;
-    }
-
-    if (direction === 'left') {
-      setCount(count - numToShow);
-      listRef.current.style.transform = `translateX(${width + distance}px)`;
-      setDistance(distance + width);
-    } else if (direction === 'right') {
-      console.log('yo');
-      setCount(count + numToShow);
-      console.log(distance - width);
-      listRef.current.style.transform = `translateX(${
-        -window.innerHeight + 48 * 2
-      }px)`;
-      setDistance(distance - width);
-    }
-  };
-*/
