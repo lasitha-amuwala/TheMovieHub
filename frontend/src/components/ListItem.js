b1e74c7bf5aae9c5ad76046e389cd78636664c28
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-10-26 19:38:15 -0400

    Fixed list navigation, improved list style, increased list button size

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,15 +1,29 @@
 import React, { forwardRef } from 'react';
 
-export const ListItem = forwardRef((props, ref) => {
-
-  return (
-    <img
-      ref={ref}
-      className="h-full w-c-1/3 sm:w-c-1/4 md:w-c-1/5 lg:w-c-1/6 xl:w-c-1/7 2xl:w-c-1/8 cursor-pointer rounded-lg"
-      src={`https://image.tmdb.org/t/p/w500/${props.data.poster_path}`}
-      alt={`${props.data.title.split(' ').join('-')}-poster`}
-    />
-  );
-});
+export const ListItem = forwardRef(({ data }, ref) => (
+	<li className='px-1 min-w-1/3 sm:min-w-1/4 md:min-w-1/5 lg:min-w-1/6 xl:min-w-1/7 2xl:min-w-1/8'>
+		<img
+			ref={ref}
+			className="h-full cursor-pointer rounded-lg "
+			src={
+				data.poster_path &&
+				`https://image.tmdb.org/t/p/w500/${data.poster_path}`
+			}
+			alt={`${data.title.split(' ').join('-')}-poster`}
+		/>
+	</li>
+));
 
 //transform hover:scale-125 hover:-translate-x-2/3 z-20 hover:z-30 transition-all duration-500 ease-in-out
+/**
+ * 
+ * 		<img
+			ref={ref}
+			className="h-full w-c-1/3 sm:w-c-1/4 md:w-c-1/5 lg:w-c-1/6 xl:w-c-1/7 2xl:w-c-1/8 cursor-pointer rounded-lg"
+			src={
+				data.poster_path &&
+				`https://image.tmdb.org/t/p/w500/${data.poster_path}`
+			}
+			alt={`${data.title.split(' ').join('-')}-poster`}
+		/>
+ */
