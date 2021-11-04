3a5fa24828151855d4250b9901bdd59aa2dbbc77
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-11-04 14:14:20 -0400

    improved swipe animation on mobile, made carousel smaller on fullscreen

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,18 +1,18 @@
 import React, { forwardRef } from 'react';
 
-export const ListItem = forwardRef(({ data }, ref) => (
-	<li className="px-1 min-w-1/3 sm:min-w-1/4 md:min-w-1/5 lg:min-w-1/6 xl:min-w-1/7 2xl:min-w-1/8">
-		<img
-			ref={ref}
-			className="h-auto cursor-pointer rounded-lg "
-			src={
-				data.poster_path &&
-				`https://image.tmdb.org/t/p/w500/${data.poster_path}`
-			}
-			alt={`${data.title.split(' ').join('-')}-poster`}
-		/>
-	</li>
-));
+export const ListItem = forwardRef(
+	({ data }, ref) =>
+		data.poster_path && (
+			<li className="px-1 min-w-1/3 sm:min-w-1/4 md:min-w-1/5 lg:min-w-1/6 xl:min-w-1/7 2xl:min-w-1/8">
+				<img
+					ref={ref}
+					className="h-auto cursor-pointer rounded-lg "
+					src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+					alt={`${data.title.split(' ').join('-')}-poster`}
+				/>
+			</li>
+		)
+);
 
 //transform hover:scale-125 hover:-translate-x-2/3 z-20 hover:z-30 transition-all duration-500 ease-in-out
 /**
