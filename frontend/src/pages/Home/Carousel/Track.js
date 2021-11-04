c858cd0feb05999c11183875a2c145d4dc046de5
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-11-04 18:27:05 -0400

    fixed warnings

--- frontend/src/pages/Home/Carousel/Track.js
+++ frontend/src/pages/Home/Carousel/Track.js
@@ -1,44 +1,32 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { usePrevious } from '../../../hooks/hooks';
+import React, { useEffect, useRef } from 'react';
 
-export const Track = ({
-	children,
-	slideIndex,
-	lastIndex,
-	animation,
-	prevIndex,
-	handleIndex,
-	handleAnimation,
-}) => {
+export const Track = ({ handleAnimation, handleIndex, ...props }) => {
 	const ref = useRef(null);
 
 	useEffect(() => {
 		if (ref.current)
-			ref.current.style.transform = `translateX(${100 * -slideIndex}%)`;
-	}, [slideIndex]);
+			ref.current.style.transform = `translateX(${100 * -props.slideIndex}%)`;
+	}, [props.slideIndex]);
 
 	const handleLoop = () => {
-		if (slideIndex === 0 && prevIndex !== 0) {
-			handleIndex(lastIndex - 1);
+		if (props.slideIndex === 0 && props.prevIndex !== 0) {
+			handleIndex(props.lastIndex - 1);
 			handleAnimation(false);
-		}
-		else if(slideIndex === lastIndex - 1){
+		} else if (props.slideIndex === props.lastIndex - 1) {
 			handleIndex(0);
 			handleAnimation(false);
 		}
 	};
 
+	const transition =
+		'transform .75s ease 0s,-webkit-transform .75s ease 0s,-moz-transform .75s ease 0s,-o-transform .75s ease 0s';
 	return (
 		<div
 			ref={ref}
 			onTransitionEnd={handleLoop}
-			style={{
-				transition:
-					animation &&
-					'transform .75s ease 0s,-webkit-transform .75s ease 0s,-moz-transform .75s ease 0s,-o-transform .75s ease 0s',
-			}}
+			style={{ transition: props.animation && transition }}
 		>
-			{children}
+			{props.children}
 		</div>
 	);
 };
