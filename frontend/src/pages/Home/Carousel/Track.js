9c702b31a1e81ba9a5ae2b2eb459db234fc342bb
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-11-04 18:19:08 -0400

    Fixed carousel loop

--- frontend/src/pages/Home/Carousel/Track.js
+++ frontend/src/pages/Home/Carousel/Track.js
@@ -1,34 +1,42 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { usePrevious } from '../../../hooks/usePrevious';
+import { usePrevious } from '../../../hooks/hooks';
 
-export const Track = ({ children, slideIndex, lastIndex, onLoop }) => {
-	const [index, setIndex] = useState(slideIndex);
-
+export const Track = ({
+	children,
+	slideIndex,
+	lastIndex,
+	animation,
+	prevIndex,
+	handleIndex,
+	handleAnimation,
+}) => {
 	const ref = useRef(null);
-	const prevIndex = usePrevious(index);
-
-	useEffect(() => setIndex(slideIndex), [slideIndex]);
 
 	useEffect(() => {
 		if (ref.current)
-			ref.current.style.transform = `translateX(${100 * -index}%)`;
-		if (index === lastIndex && prevIndex === 0) onLoop(lastIndex - 1);
-	}, [index]);
+			ref.current.style.transform = `translateX(${100 * -slideIndex}%)`;
+	}, [slideIndex]);
 
 	const handleLoop = () => {
-		if (index === lastIndex && prevIndex !== 0) onLoop(0);
+		if (slideIndex === 0 && prevIndex !== 0) {
+			handleIndex(lastIndex - 1);
+			handleAnimation(false);
+		}
+		else if(slideIndex === lastIndex - 1){
+			handleIndex(0);
+			handleAnimation(false);
+		}
 	};
 
 	return (
 		<div
 			ref={ref}
 			onTransitionEnd={handleLoop}
-			className={
-				(index === 0 && prevIndex === lastIndex) ||
-				(index === lastIndex && prevIndex === lastIndex)
-					? ''
-					: 'transition duration-300 md:duration-700 ease-in-out'
-			}
+			style={{
+				transition:
+					animation &&
+					'transform .75s ease 0s,-webkit-transform .75s ease 0s,-moz-transform .75s ease 0s,-o-transform .75s ease 0s',
+			}}
 		>
 			{children}
 		</div>
