be0b9ece7e6ede7cf26a2c47276e040f6b2ec12c
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-11-04 18:19:08 -0400

    Fixed carousel loop

--- frontend/src/hooks/hooks.js
+++ frontend/src/hooks/hooks.js
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from 'react';
 
-// custome hook created by Dan Abramov
-// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-
 export const useInterval = (callback, delay) => {
+	// custome hook created by Dan Abramov
+	// https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+
 	const savedCallback = useRef();
 
 	// Remember the latest callback.
@@ -21,4 +21,10 @@ export const useInterval = (callback, delay) => {
 			return () => clearInterval(id);
 		}
 	}, [delay]);
+};
+
+export const usePrevious = (value) => {
+	const ref = useRef(null);
+	useEffect(() => (ref.current = value), [value]);
+	return ref.current;
 };
--- frontend/src/pages/Home/Carousel/Carousel.js
+++ frontend/src/pages/Home/Carousel/Carousel.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useInterval } from '../../../hooks/useInterval';
+import { useInterval, usePrevious } from '../../../hooks/hooks';
 
 import { ReactComponent as ChevronRight } from '../../../icons/chevronRight.svg';
 import { ReactComponent as ChevronLeft } from '../../../icons/chevronLeft.svg';
@@ -10,33 +10,45 @@ import { useSwipeable } from 'react-swipeable';
 
 export const Carousel = ({ slides, autoplay }) => {
 	const [sliderData, setSliderData] = useState([]);
-	const [slideIndex, setSlideIndex] = useState(0);
+	const [slideIndex, setSlideIndex] = useState(1);
+	const [animation, setAnimation] = useState(false);
 	const [lastIndex, setLastIndex] = useState(0);
 	const [delay, setDelay] = useState(10000);
 
-	// Add first element to the end of the list to create an infinite carousel
-	useEffect(() => {
-		if (slides.length > 0) {
-			let list = slides.concat([slides[0]]);
-			setSliderData(list);
-			setLastIndex(list.length - 1);
-		}
-	}, [slides]);
-
+	const prevIndex = usePrevious(slideIndex);
 	// enable auto play every set interval
 	useInterval(() => handleClick('right'), autoplay ? delay : null);
-
-	const handleClick = (direction) => {
-		setDelay(delay + 1);
-		direction === 'left'
-			? setSlideIndex((idx) => (idx <= 0 ? lastIndex : idx - 1))
-			: setSlideIndex((idx) => (idx >= lastIndex ? 0 : idx + 1));
-	};
 
 	const handlers = useSwipeable({
 		onSwipedLeft: () => handleClick('right'),
 		onSwipedRight: () => handleClick('left'),
 	});
+
+	// Add first element to the end of the list to create an infinite carousel
+	useEffect(() => {
+		if (slides.length > 0) {
+			let list = slides.concat(slides[0]);
+			list.unshift(list[list.length - 2]);
+			setSliderData(list);
+			setLastIndex(list.length - 1);
+		}
+	}, [slides]);
+
+	const handleClick = (direction) => {
+		(slideIndex === 0 && prevIndex === lastIndex) ||
+		(slideIndex === lastIndex && prevIndex === lastIndex)
+			? setAnimation(false)
+			: setAnimation(true);
+
+		setDelay(delay + 1);
+
+		direction === 'left'
+			? setSlideIndex((idx) => (idx <= 0 ? lastIndex : idx - 1))
+			: setSlideIndex((idx) => (idx >= lastIndex ? 0 : idx + 1));
+	};
+
+	const handleIndex = (i) => setSlideIndex(i);
+	const handleAnimation = (i) => setAnimation(i);
 
 	// if the datails not an array return null
 	if (!Array.isArray(sliderData) || !sliderData.length) return null;
@@ -66,9 +78,12 @@ export const Carousel = ({ slides, autoplay }) => {
 			</SliderButton>
 			<div {...handlers} className="w-full overflow-hidden relative">
 				<Track
+					animation={animation}
 					slideIndex={slideIndex}
 					lastIndex={lastIndex}
-					onLoop={(index) => setSlideIndex(index)}
+					prevIndex={prevIndex}
+					handleIndex={handleIndex}
+					handleAnimation={handleAnimation}
 				>
 					<ul className="flex relative list-none">
 						{sliderData.map((data) => (
