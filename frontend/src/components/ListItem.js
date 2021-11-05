0b095d3ef83134b23e9fd06607bc532539e862af
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-11-04 20:28:09 -0400

    Formate code

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -13,17 +13,3 @@ export const ListItem = forwardRef(
 			</li>
 		)
 );
-
-//transform hover:scale-125 hover:-translate-x-2/3 z-20 hover:z-30 transition-all duration-500 ease-in-out
-/**
- * 
- * 		<img
-			ref={ref}
-			className="h-full w-c-1/3 sm:w-c-1/4 md:w-c-1/5 lg:w-c-1/6 xl:w-c-1/7 2xl:w-c-1/8 cursor-pointer rounded-lg"
-			src={
-				data.poster_path &&
-				`https://image.tmdb.org/t/p/w500/${data.poster_path}`
-			}
-			alt={`${data.title.split(' ').join('-')}-poster`}
-		/>
- */
