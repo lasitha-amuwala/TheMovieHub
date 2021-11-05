a6b1bb6c0be795585e85169d7f8aa56053aa7d31
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-11-05 18:14:55 -0400

    Added React Router and details page

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,15 +1,19 @@
 import React, { forwardRef } from 'react';
+import { Link } from 'react-router-dom';
 
-export const ListItem = forwardRef(
-	({ data }, ref) =>
+export const ListItem = forwardRef(({ data }, ref) => {
+	return (
 		data.poster_path && (
 			<li className="px-1 min-w-1/3 sm:min-w-1/4 md:min-w-1/5 lg:min-w-1/6 xl:min-w-1/7 2xl:min-w-1/8">
-				<img
-					ref={ref}
-					className="h-auto cursor-pointer rounded-lg "
-					src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
-					alt={`${data.title.split(' ').join('-')}-poster`}
-				/>
+				<Link to={`/details/${data.id}/movie`}>
+					<img
+						ref={ref}
+						className="h-auto cursor-pointer rounded-lg "
+						src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+						alt={`${data.title.split(' ').join('-')}-poster`}
+					/>
+				</Link>
 			</li>
 		)
-);
+	);
+});
