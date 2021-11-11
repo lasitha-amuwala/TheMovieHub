870d15d11f08cca8821cbef1976e3bebb3a13c6c
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-11-11 14:43:16 -0500

    Added details page and routing, nabvar navigation

--- backend/src/routes/tmdb.js
+++ backend/src/routes/tmdb.js
@@ -2,9 +2,10 @@ const createRouter = require('./router');
 const controller = require('../controllers/tmdb');
 
 module.exports = createRouter([
-  ['get', '/trending', controller.getTrending],
-  ['get', '/nowPlaying', controller.getNowPlaying],
-  ['get', '/popular', controller.getPopular],
-  ['get', '/topRated', controller.getTopRated],
-  ['get', '/upcoming', controller.getUpcoming],
+	['get', '/trending', controller.getTrending],
+	['get', '/nowPlaying', controller.getNowPlaying],
+	['get', '/popular', controller.getPopular],
+	['get', '/topRated', controller.getTopRated],
+	['get', '/upcoming', controller.getUpcoming],
+	['get', '/movie/:id', controller.getMovieById],
 ]);
