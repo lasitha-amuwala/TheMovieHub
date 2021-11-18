a2ddf2ee39c3a7bfb3f5862ffeee2b43923cca70
Lasitha Amuwala <lasithaamuwala@gmail.com>
2021-11-17 23:57:44 -0500

    added poster and backdrop to detials page

--- frontend/src/pages/Movie.js
+++ frontend/src/pages/Movie.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import { getMovie } from '../api/tmdb';
 import { useParams } from 'react-router';
-import { Navbar } from '../components/Navbar';
 
 export const Movie = () => {
-	const { id, type } = useParams();
+	const { id } = useParams();
 	const [data, setData] = useState({});
 
 	useEffect(() => getMovie(id).then((res) => setData(res)), [id]);
 
 	return (
 		<div>
-			<div className="w-96">
-				<img src={`https://image.tmdb.org/t/p/original/${data.poster_path}`} />
+			<div
+				className="w-full  bg-contain "
+				style={{
+					backgroundImage: `url(https://image.tmdb.org/t/p/original/${data.backdrop_path})`,
+				}}
+			>
+				<div className="grid grid-cols-2 h-full bg-black bg-opacity-80">
+					<div className="h-full w-full">
+						<img
+							src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+						></img>
+						hi
+					</div>
+					<div className="h-full w-full"></div>
+				</div>
 			</div>
+			<div className="h-screen">hm</div>
 		</div>
 	);
 };
+
+//					src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
