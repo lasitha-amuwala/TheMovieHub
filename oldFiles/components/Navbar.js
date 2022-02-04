8aa7081f696e32a96715f75c13d2b892583d0eb8
Lasitha Amuwala <65428406+lasitha-amuwala@users.noreply.github.com>
2022-02-04 10:46:05 -0500

    Copied old files to oldFiles folder

--- oldFiles/components/Navbar.js
+++ oldFiles/components/Navbar.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export const Navbar = () => (
 	<div className="h-16 fixed w-full top-0 bg z-50 bg-almostBlack bg-opacity-80 backdrop-filter backdrop-blur">
 		<div className="flex h-full items-center justify-between mx-7% md:mx-5%">
 			<div className="flex items-center text-white">
-				<Link to={'/'}>
 					<h6 className="font-bold">MovieHub</h6>
-				</Link>
 				<div className="flex pl-8 gap-4">
 					<button className="px-4 py-2 hover:text-gray-300 font-normal">
 						Movies
