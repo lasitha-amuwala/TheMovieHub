cbe988c0efeae530e774edae9ca9742ae54c389a
Lasitha Amuwala <65428406+lasitha-amuwala@users.noreply.github.com>
2022-02-06 17:47:12 -0500

    Added details json filtering

--- frontend/pages/details/[id].js
+++ frontend/pages/details/[id].js
@@ -14,31 +14,30 @@ export const getStaticProps = async ({ params }) => {
 
 	try {
 		const res = await fetch(req);
-		const data = await res.json();
+		let data = await res.json();
+
+		let release_dates = data.release_dates.results.find(
+			(elem) => elem.iso_3166_1 == 'US'
+		);
+
+		// filter thorugh the list of ratings and return the latest
+		let rating = release_dates.release_dates.reduce((a, b) => {
+			return new Date(a.release_date) > new Date(b.release_date) ? a : b;
+		});
+
+		delete data.release_dates;
+		delete data.production_companies;
+		delete data.production_countries;
+		delete data.spoken_languages;
+		delete data.adult;
+
+		data.rating = rating.certification;
+
 		return { props: data };
 	} catch (e) {
-		console.error(error);
+		console.error(e);
 		return { notFound: true };
 	}
-	/*
-	let release_dates = data.release_dates.results.find(
-		(elem) => elem.iso_3166_1 == 'US'
-	);
-
-	// filter thorugh the list of ratings and return the latest
-	let rating = release_dates.release_dates.reduce((a, b) => {
-		return new Date(a.release_date) > new Date(b.release_date) ? a : b;
-	});
-
-	// remove unnessesary data
-	delete data.release_dates;
-	delete data.production_companies;
-	delete data.production_countries;
-	delete data.spoken_languages;
-	delete data.adult;
-
-	data.rating = rating.certification;
-*/
 };
 
 const Details = (data) => {
@@ -57,16 +56,29 @@ const Details = (data) => {
 				}}>
 				<div className='flex bg-black bg-opacity-90 h-40vh 2xl:px-20%'>
 					<div className='relative flex-none px-10 py-10'>
-					<Image
+						<Image
 							layout='fill'
 							objectFit='cover'
-							objectPosition=''
 							className='h-full rounded-xl'
 							src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
 							alt={`${data.title}-poster`}
 						/>
 					</div>
-
+					<div className='p-10 flex flex-col flex-1 gap-5 text-4xl text-white'>
+						<div className='font-bold'>
+							{data.title}
+							<span className='font-light pl-4'>{`(${
+								data.release_date.split('-')[0]
+							})`}</span>
+						</div>
+						<div className='flex gap-3 items-center'>
+							<div className='text-sm border p-1 px-2 border-gray-400 text-gray-400 inline'>
+								{data.rating}
+							</div>
+							<div className='text-base'>{formatRuntime(data.runtime)}</div>
+						</div>
+						<div className='font-normal text-lg'>{data.overview}</div>
+					</div>
 				</div>
 			</div>
 			<div className='h-screen'></div>
