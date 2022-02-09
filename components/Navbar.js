f043c8703687d22659a08758a2b8e8ce05b74d1e
Lasitha Amuwala <65428406+lasitha-amuwala@users.noreply.github.com>
2022-02-09 11:29:12 -0500

    Improved Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
 
-export const Navbar = () => (
-	<div className='h-16 fixed w-full top-0 bg z-50 bg-almostBlack bg-opacity-80 backdrop-filter backdrop-blur'>
-		<div className='flex h-full items-center justify-between mx-7% md:mx-5%'>
-			<div className='flex items-center text-white'>
-				<Link href='/' className='w-full cursor-pointer' passHref>
-					<h6 className='px-4 py-2 font-bold '>MovieHub</h6>
-				</Link>
-				<div className='flex pl-8 gap-4'>
-					<button className='px-4 py-2 hover:text-gray-300 font-normal'>
-						Movies
-					</button>
-					<button className='px-4 py-2 hover:text-gray-300 font-normal'>
-						Series
-					</button>
-					<button className='px-4 py-2 hover:text-gray-300 font-normal'>
-						Profile
-					</button>
-					<button className='px-4 py-2 hover:text-gray-300 font-normal'>
-						My List
-					</button>
+export const Navbar = () => {
+
+	const NavItem = ({ label, to }) => (
+		<Link href={to} className='px-4 py-2 hover:text-gray-300'>
+			<a>{label}</a>
+		</Link>
+	);
+
+	return (
+		<div className='h-16 fixed w-full top-0 bg z-50 bg-almostBlack bg-opacity-80 backdrop-filter backdrop-blur'>
+			<div className='flex h-full items-center justify-between mx-7% md:mx-5%'>
+				<div className='flex items-center text-white font-bold'>
+					<NavItem to='/' label='MovieHub'/>
+					<div className='flex pl-8 gap-4 font-normal'>
+						<NavItem label='Movies' />
+						<NavItem label='Series' />
+						<NavItem label='Profile' />
+						<NavItem label='My List' />
+					</div>
 				</div>
 			</div>
-			<button className='text-gray-700 px-4'></button>
 		</div>
-	</div>
-);
+	)
+};
