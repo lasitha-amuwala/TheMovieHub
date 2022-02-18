070c7366ac93315e8b6722c64e851348aab77c33
Lasitha Amuwala <65428406+lasitha-amuwala@users.noreply.github.com>
2022-02-18 05:33:25 -0500

    Added more movei detail, css improvements

--- pages/details/[id].js
+++ pages/details/[id].js
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 
 const Details = (data) => {
   const router = useRouter();
-
+  console.log(data);
   const formatRuntime = (mins) => {
     let min = mins % 60;
     let h = (mins - min) / 60;
@@ -19,7 +19,7 @@ const Details = (data) => {
       <Head>
         <title>{`${data.title} - ${process.env.title}`}</title>
       </Head>
-      <div className='max-h-none h-full w-full lg:h-[40vh] lg:max-h-40vh lg:overflow-hidden '>
+      <div className='max-h-none h-full w-full lg:overflow-hidden '>
         <div className='relative h-full w-full'>
           <Image
             layout='fill'
@@ -31,40 +31,51 @@ const Details = (data) => {
             blurDataURL={`https://image.tmdb.org/t/p/w500/${data.backdrop_path}`}
           />
           <div className='z-1 relative h-full'>
-            <div className='h-full bg-black bg-opacity-70'>
-              <div className='m-auto flex h-full max-w-[var(--maxPageWidth)] flex-col gap-16 p-10 lg:flex-row'>
-                <div className='block h-full w-[calc(40vh*0.7)] flex-none self-center rounded-xl border-2 border-cyan-200 lg:w-[calc((40vh-80px)*0.65)]'>
+            <div className='h-full bg-black bg-opacity-50 backdrop-blur-3xl backdrop-filter'>
+              <div className='m-auto flex h-full max-w-[var(--maxPageWidth)] flex-col gap-10 p-10 lg:flex-row lg:gap-20 lg:p-20'>
+                <div className='block h-full w-[calc(40vh*0.7)] flex-none self-center rounded-xl lg:w-[calc((40vh-80px)*0.65)]'>
                   <Image
-                    width={1080}
-                    height={1669}
+                    width={500}
+                    height={750}
                     layout='responsive'
                     className='rounded-xl'
                     src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
                     alt={`${data.title}-poster`}
                     quality={100}
-                    placeholder='blur'
-                    blurDataURL={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
                   />
                 </div>
-                <div className='flex flex-col gap-4 text-4xl text-white'>
+                <div className='flex flex-col gap-6 py-5 text-white lg:text-5xl'>
                   <div className='flex flex-wrap items-center gap-3 lg:gap-4'>
-                    <div className='font-bold'>{data.title}</div>
-                    <div className='text-3xl font-light text-gray-300'>
+                    <div className='text-4xl font-bold '>{data.title}</div>
+                    <div className='text-4xl font-light text-gray-300'>
                       {`(${data.release_date.split('-')[0]})`}
                     </div>
                   </div>
-                  <div className='flex gap-4'>
+                  <div className='flex flex-wrap items-center gap-3 text-base lg:gap-4'>
                     {data.rating && (
-                      <div className='inline border border-gray-400 p-1 px-2 text-sm text-gray-400'>
+                      <div className='inline border border-gray-300 py-[0.3rem] px-2 text-sm leading-none text-gray-300'>
                         {data.rating}
                       </div>
                     )}
-                    <div className='text-base'>
-                      {formatRuntime(data.runtime)}
+                    <div>{formatRuntime(data.runtime)}</div>
+                    <div className='flex gap-4'>
+                      {data.genres.map(({ name }) => (
+                        <div className="sm:before:pr-3 sm:before:content-['\2022']">
+                          {name}
+                        </div>
+                      ))}
                     </div>
                   </div>
-                  <div className='text-base font-normal md:text-lg'>
-                    {data.overview}
+                  {data.tagline && (
+                    <div className='font-normal italic text-gray-300 md:text-lg'>
+                      {data.tagline}
+                    </div>
+                  )}
+                  <div>
+                    <div className='pb-2 text-xl font-bold'>Overview</div>
+                    <div className='text-base font-normal md:text-lg'>
+                      {data.overview}
+                    </div>
                   </div>
                 </div>
               </div>
