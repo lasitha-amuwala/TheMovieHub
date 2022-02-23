26eb80060bfff891691f7566ce48c3a7610155b0
Lasitha Amuwala <65428406+lasitha-amuwala@users.noreply.github.com>
2022-02-23 13:27:04 -0500

    Added ReactQuery to home page

--- pages/index.js
+++ pages/index.js
@@ -2,58 +2,42 @@ import React from 'react';
 import Head from 'next/head';
 import { List } from '../components/List';
 import { Carousel } from '../components/Carousel/Carousel';
-
-const Home = (props) => (
-  <div className='relative min-h-screen overflow-hidden'>
-    <Head>
-      <title>{process.env.title}</title>
-    </Head>
-    <Carousel slides={props.trendingList.results} autoplay />
-    <div className='flex flex-col gap-9'>
-      <List data={props.popularList.results} title='Popular' />
-      <List data={props.topRatedList.results} title='Top Rated' />
-      <List data={props.nowPlayingList.results} title='Now Playing' />
-      <List data={props.upcomingList.results} title='Upcoming' />
-    </div>
-  </div>
-);
-
-export default Home;
+import { QueryClient, useQuery, dehydrate } from 'react-query';
+import { apiQueries } from '../utils/http-client/apiQueries';
 
 export const getServerSideProps = async () => {
-  const baseURL = 'https://api.themoviedb.org/3';
-  const API_KEY = `api_key=${process.env.TMDB_API_KEY}`;
-
-  const [trendingRes, nowPlayingRes, popularRes, topRatedRes, upcomingRes] =
-    await Promise.all([
-      fetch(`${baseURL}/trending/movie/week?${API_KEY}`),
-      fetch(`${baseURL}/movie/now_playing?region=US&language=en-US&${API_KEY}`),
-      fetch(`${baseURL}/movie/popular?region=US&language=en-US&${API_KEY}`),
-      fetch(`${baseURL}/movie/top_rated?region=US&language=en-US&${API_KEY}`),
-      fetch(`${baseURL}/movie/upcoming?region=US&language=en-US&${API_KEY}`),
-    ]);
-
-  const [
-    trendingList,
-    nowPlayingList,
-    popularList,
-    topRatedList,
-    upcomingList,
-  ] = await Promise.all([
-    trendingRes.json(),
-    nowPlayingRes.json(),
-    popularRes.json(),
-    topRatedRes.json(),
-    upcomingRes.json(),
+  const queryClient = new QueryClient();
+  await Promise.all([
+    queryClient.fetchQuery(apiQueries.trending.movies()),
+    queryClient.fetchQuery(apiQueries.movies.nowPlaying()),
+    queryClient.fetchQuery(apiQueries.movies.popular()),
+    queryClient.fetchQuery(apiQueries.movies.topRated()),
+    queryClient.fetchQuery(apiQueries.movies.upcoming()),
   ]);
 
-  return {
-    props: {
-      trendingList,
-      nowPlayingList,
-      popularList,
-      topRatedList,
-      upcomingList,
-    },
-  };
+  return { props: { dehydratedState: dehydrate(queryClient) } };
 };
+
+const Home = () => {
+  const { data: trendingList } = useQuery(apiQueries.trending.movies());
+  const { data: nowPlayingList } = useQuery(apiQueries.movies.nowPlaying());
+  const { data: popularList } = useQuery(apiQueries.movies.popular());
+  const { data: topRatedList } = useQuery(apiQueries.movies.topRated());
+  const { data: upcomingList } = useQuery(apiQueries.movies.upcoming());
+
+  return (
+    <div className='relative min-h-screen overflow-hidden'>
+      <Head>
+        <title>{process.env.title}</title>
+      </Head>
+      <Carousel slides={trendingList.results} autoplay />
+      <div className='flex flex-col gap-9'>
+        <List data={popularList.results} title='Popular' />
+        <List data={topRatedList.results} title='Top Rated' />
+        <List data={nowPlayingList.results} title='Now Playing' />
+        <List data={upcomingList.results} title='Upcoming' />
+      </div>
+    </div>
+  );
+};
+export default Home;
--- utils/http-client/apiQueries.js
+++ utils/http-client/apiQueries.js
@@ -1,14 +1,40 @@
 import { httpClient } from './httpClient';
 import { createUrl } from './httpClientUtils';
+const { get } = httpClient;
+
+const USParams = { region: 'US', language: 'en-US' };
 
 export const apiQueries = {
   movies: {
     movie: (id) => ({
       queryKey: ['movies', id],
       queryFn: () =>
-        httpClient.get(
-          createUrl(`/movie/${id}`, { append_to_response: 'release_dates' })
+        get(createUrl(`/movie/${id}`, { append_to_response: 'release_dates' })),
+    }),
+    nowPlaying: () => ({
+      queryKey: ['movies', 'nowPlaying'],
+      queryFn: () =>
+        get(
+          createUrl(`/movie/now_playing`, { region: 'US', language: 'en-US' })
         ),
+    }),
+    popular: () => ({
+      queryKey: ['movies', 'popular'],
+      queryFn: () => get(createUrl(`/movie/popular`, USParams)),
+    }),
+    topRated: () => ({
+      queryKey: ['movies', 'topRated'],
+      queryFn: () => get(createUrl(`/movie/top_rated`, USParams)),
+    }),
+    upcoming: () => ({
+      queryKey: ['movies', 'upcoming'],
+      queryFn: () => get(createUrl(`/movie/upcoming`, USParams)),
+    }),
+  },
+  trending: {
+    movies: () => ({
+      queryKey: ['trending', 'movies'],
+      queryFn: () => get(createUrl('/trending/movie/week')),
     }),
   },
 };
