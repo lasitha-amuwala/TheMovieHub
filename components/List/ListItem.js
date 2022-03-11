35a35a5b3ecd1967d2753934b991bfa3a2f2c1dd
Lasitha Amuwala <65428406+lasitha-amuwala@users.noreply.github.com>
2022-03-11 02:47:33 -0500

    Added movie card info + css improvements

--- components/List/ListItem.js
+++ components/List/ListItem.js
@@ -2,6 +2,7 @@ import { useState, forwardRef, useCallback } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import useApiConfiguration from '../../src/useApiConfig';
+import { MdOutlineStar } from 'react-icons/md';
 
 const ListItem = forwardRef(({ data, index, numPerList, onItemHover }, ref) => {
   const { getImageUrl } = useApiConfiguration();
@@ -21,18 +22,29 @@ const ListItem = forwardRef(({ data, index, numPerList, onItemHover }, ref) => {
       >
         <Link href={`/details/${data.id}`}>
           <a>
-            <div className='list2 relative h-full overflow-hidden rounded-lg sm:hover:ring-2 sm:hover:ring-borderPrimary'>
-              <div className='item-cover hidden h-full bg-backgroundShadow sm:block '>
-                <Image
-                  width={16}
-                  height={9}
-                  layout='responsive'
-                  className='rounded-t-lg'
-                  objectFit='cover'
-                  src={getImageUrl(data.backdrop_path, { original: false })}
-                  alt={`${data.title.split(' ').join('-')}-poster`}
-                />
-                <div className='p-1 px-3 text-lg font-medium'>{data.title}</div>
+            <div className='list2 relative h-full overflow-hidden rounded-lg transition duration-200 sm:hover:ring-2 sm:hover:ring-borderPrimary'>
+              <div className='item-cover flex h-full flex-col bg-backgroundShadow'>
+                <div className='hidden sm:block '>
+                  <Image
+                    width={16}
+                    height={8.5}
+                    layout='responsive'
+                    className='rounded-t-lg'
+                    objectFit='cover'
+                    src={getImageUrl(data.backdrop_path, { original: false })}
+                    alt={`${data.title.split(' ').join('-')}-poster`}
+                  />
+                </div>
+                <div className='flex h-full flex-col justify-center p-1 px-3 2xl:p-3 2xl:px-4 3xl:px-5 text-sm md:text-base xl:text-lg 2xl:text-xl'>
+                  <div className='grow font-medium line-clamp-1'>{data.title}</div>
+                  <div className='flex grow items-center justify-between  text-gray-300'>
+                    <div>{data.release_date.split('-')[0]}</div>
+                    <div className='flex items-center gap-2'>
+                      <MdOutlineStar className='fill-yellow-400' />
+                      <span className='font-semibold text-slate-200'>{data.vote_average}</span> / 10
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className='item-poster absolute top-0 block h-full w-full '>
                 <Image
