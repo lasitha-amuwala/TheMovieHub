a3cbd67cd74d14f205f1fa8eff4baf6f60546779
Lasitha Amuwala <65428406+lasitha-amuwala@users.noreply.github.com>
2022-03-25 04:48:43 -0400

    name changes

--- pages/movie/[movieId].js
+++ pages/movie/[movieId].js
@@ -6,9 +6,9 @@ import { QueryClient, dehydrate, useQuery } from 'react-query';
 import Title from '../../components/Title';
 import VideoModal from '../../components/VideoModal';
 import PageMargin from '../../components/PageMargin';
-import MovieHeader from '../../components/movie/MovieHeader';
-import MovieCastCarousel from '../../components/movie/MovieCastCarousel';
-import MovieVideoCarousel from '../../components/movie/MovieVideoCarousel';
+import MovieHeader from '../../components/Movie/MovieHeader';
+import MovieCastCarousel from '../../components/Movie/MovieCastCarousel';
+import MovieVideoCarousel from '../../components/Movie/MovieVideoCarousel';
 
 export const getServerSideProps = async ({ params }) => {
   try {
