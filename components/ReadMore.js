f6243284a7ce86771501009043920e007c26f923
Lasitha Amuwala <65428406+lasitha-amuwala@users.noreply.github.com>
2022-03-28 04:35:43 -0400

    Created ReadMore Component

---
+++ components/ReadMore.js
@@ -0,0 +1,43 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+const ReadMore = ({ lines, text, bg }) => {
+  const [truncated, setTruncated] = useState(true);
+  const [isEnabled, setIsEnabled] = useState(true);
+
+  const textRef = useRef(null);
+
+  const style = {
+    overflow: 'hidden',
+    display: '-webkit-box',
+    '-webkit-box-orient': 'vertical',
+    '-webkit-line-clamp': `${lines}`,
+  };
+
+  useEffect(() => {
+    if (textRef.current && textRef.current.clientHeight == textRef.current.scrollHeight) {
+      setIsEnabled(false);
+    }
+  }, []);
+
+  if (!isEnabled) return <div>{text}</div>;
+
+  return (
+    <div className='relative'>
+      <div ref={textRef} style={truncated ? style : null}>
+        {text}
+      </div>
+      <div className={`${truncated && 'absolute bottom-0'} w-full`}>
+        <div className={`flex justify-end bg-gradient-to-r from-transparent to-${bg}`}>
+          <button
+            onClick={() => setTruncated(!truncated)}
+            className={`bg-${bg} rounded-lg py-0 px-2 font-bold text-accentBlue transition-colors duration-150 hover:text-accentBlueHover`}
+          >
+            {truncated ? 'Read More' : 'Show Less'}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReadMore;
