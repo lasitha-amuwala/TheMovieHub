ba4eab3474a8987dbcc2b43bb089a2c1e9a27bdc
lasitha-amuwala <lasithaamuwala@gmail.com>
2025-09-24 17:43:56 -0400

    Removed old components

--- src/components/Spinner.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { CgSpinner } from 'react-icons/cg';
-
-export const Spinner = () => (
-  <CgSpinner className='m-auto h-full w-14 animate-spin text-slate-300' />
-);
