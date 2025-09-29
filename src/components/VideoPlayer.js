764172ae80f1d18199590394467f265351208370
lasitha-amuwala <lasithaamuwala@gmail.com>
2025-09-29 02:23:32 -0400

    fixed video player

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 
-const VideoPlayer = ({ videoId }) => {
-  return (
-    <ReactPlayer
-      controls
-      playsinline
-      url={`https://www.youtube.com/watch?v=${videoId}`}
-      width='100%'
-      height='100%'
-    />
-  );
-};
+const VideoPlayer = ({ videoId }) => (
+  <ReactPlayer
+    controls
+    playsinline
+    src={`https://www.youtube.com/watch?v=${videoId}`}
+    width='100%'
+    height='100%'
+  />
+);
 
 export default VideoPlayer;
