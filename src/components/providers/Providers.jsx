44951dccd74a3246c9311bacb8d3d4c260b719ca
lasitha-amuwala <lasithaamuwala@gmail.com>
2025-09-30 02:56:56 -0400

    Removed Lenis smooth scroll

--- src/components/providers/Providers.jsx
+++ src/components/providers/Providers.jsx
@@ -1,10 +1,7 @@
 import QueryProvider from '@/components/providers/QueryProvider';
-import { ScrollContext } from '@/components/ScrollContext';
 
 export const Providers = ({ children }) => (
   <>
-    <QueryProvider>
-      <ScrollContext>{children}</ScrollContext>
-    </QueryProvider>
+    <QueryProvider>{children}</QueryProvider>
   </>
 );
